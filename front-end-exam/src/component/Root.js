02059584a0933b0de9493a4ebe8f633a24d0ce6c
Justin <koala799222@gmail.com>
2022-01-19 09:57:19 +0800

    add navbar layout

--- front-end-exam/src/component/Root.js
+++ front-end-exam/src/component/Root.js
@@ -1,11 +1,24 @@
 import "../css/Root.scss"
 import React from "react"
+import logo from "../asset/logo.png"
+import homeSvgImage from "../asset/fa-solid_pencil-ruler.svg"
+import tagsSvgImage from "../asset/fa-solid_pencil-ruler_black.svg"
 
 function Root() {
   return (
     <div className="Root">
       {/* pageLayout   */}
-      <div className="navbar">NavBar</div>
+      <div className="navbar">
+        <img src={logo} className="navbar_logo" alt="navbar_logo" />
+        <div className="navbar_home">
+          <img src={homeSvgImage} alt="navbar_home" />
+          <span>Home</span>
+        </div>
+        <div className="navbar_tags">
+          <img src={tagsSvgImage} alt="navbar_tags" />
+          <span>Tags</span>
+        </div>
+      </div>
       <div className="content-wrapper">
         <div className="home-content">Home Content</div>
         <div className="foller-wrapper">Follower Wrapper</div>
